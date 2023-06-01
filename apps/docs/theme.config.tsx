491870187f30614d146a9c125035cc420240526c
kolynzb <collinsbenda360@gmail.com>
2023-06-01 14:20:16 +0300

    feat: add logo

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,18 +1,83 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const logo = (
+  <span>
+    <img
+      src="https://github.com/kolynzb/Grimoire/raw/main/assets/logo.png"
+      style={{
+        height: "40px",
+      }}
+      alt=""
+    />
+    <p
+      style={{
+        fontSize: "25px",
+      }}
+    >
+      Grimoire
+    </p>
+    <style jsx>{`
+      span {
+        padding: 0.5rem 0.5rem 0.5rem 0;
+        mask-image: linear-gradient(
+          60deg,
+          black 25%,
+          rgba(0, 0, 0, 0.2) 50%,
+          black 75%
+        );
+        mask-size: 400%;
+        mask-position: 0%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      span:hover {
+        mask-position: 100%;
+        transition: mask-position 1s ease, -webkit-mask-position 1s ease;
+      }
+    `}</style>
+  </span>
+);
 
 const config: DocsThemeConfig = {
-  logo: <span>Grimoire</span>,
+  logo,
   project: {
-    link: 'https://github.com/kolynzb/Grimoire',
+    link: "https://github.com/kolynzb/Grimoire",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/kolynzb/Grimoire/tree/main/site',
+  docsRepositoryBase: "https://github.com/kolynzb/Grimoire/tree/main/site",
   footer: {
-    text: 'Grimoire',
+    text: (
+      <div className="flex w-full flex-col items-center sm:items-start">
+        <div>
+          <a
+            className="flex items-center gap-1 text-current"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="vercel.com homepage"
+            href="https://vercel.com?utm_source=nextra.site"
+          >
+            <span>Powered by</span>
+            <svg height={20} viewBox="0 0 283 64" fill="none">
+              <title>Vercel</title>
+              <path
+                fill="currentColor"
+                d="M141.04 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.46 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM248.72 16c-11.04 0-19 7.2-19 18s8.96 18 20 18c6.67 0 12.55-2.64 16.19-7.09l-7.65-4.42c-2.02 2.21-5.09 3.5-8.54 3.5-4.79 0-8.86-2.5-10.37-6.5h28.02c.22-1.12.35-2.28.35-3.5 0-10.79-7.96-17.99-19-17.99zm-9.45 14.5c1.25-3.99 4.67-6.5 9.45-6.5 4.79 0 8.21 2.51 9.45 6.5h-18.9zM200.24 34c0 6 3.92 10 10 10 4.12 0 7.21-1.87 8.8-4.92l7.68 4.43c-3.18 5.3-9.14 8.49-16.48 8.49-11.05 0-19-7.2-19-18s7.96-18 19-18c7.34 0 13.29 3.19 16.48 8.49l-7.68 4.43c-1.59-3.05-4.68-4.92-8.8-4.92-6.07 0-10 4-10 10zm82.48-29v46h-9V5h9zM36.95 0L73.9 64H0L36.95 0zm92.38 5l-27.71 48L73.91 5H84.3l17.32 30 17.32-30h10.39zm58.91 12v9.69c-1-.29-2.06-.49-3.2-.49-5.81 0-10 4-10 10V51h-9V17h9v9.2c0-5.08 5.91-9.2 13.2-9.2z"
+              />
+            </svg>
+          </a>
+        </div>
+        <p className="mt-6 text-xs">
+          © {new Date().getFullYear()} The Nextra Project.
+        </p>
+      </div>
+    ),
   },
-}
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+};
 
-export default config
+export default config;
