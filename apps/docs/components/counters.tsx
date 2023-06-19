0a0d8946d33850bd03fc0679a68f73495adea46c
kolynzb <collinsbenda360@gmail.com>
2023-06-19 13:05:33 +0300

    chore: add tailwind counter

--- apps/docs/components/counters.tsx
+++ apps/docs/components/counters.tsx
@@ -1,24 +1,26 @@
 // Example from https://beta.reactjs.org/learn
 
-import { useState } from 'react'
-import styles from './counters.module.css'
+import { useState } from "react";
 
 function MyButton() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   function handleClick() {
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
   return (
     <div>
-      <button onClick={handleClick} className={styles.counter}>
+      <button
+        onClick={handleClick}
+        className="rounded-md px-2 py-6   border-2  mt-9 border-white "
+      >
         Clicked {count} times
       </button>
     </div>
-  )
+  );
 }
 
 export default function MyApp() {
-  return <MyButton />
+  return <MyButton />;
 }
